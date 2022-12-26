8f8ae9a77dde3bfcdf0200d91f24799f7cd080b1
Joao Felipe <joaofelipenmoura@gmail.com>
2022-12-26 09:17:15 -0300

    adicionado uma o input para que o usuario consiga utilizar

--- imc.py
+++ imc.py
@@ -1,6 +1,10 @@
-name = "João Felipe"
-height = 64
-Weight = 1.70
-imc = Weight / height ** 2
-result = f'João Felipe tem um imc de {imc:.2f}'
+name = "Qual é o seu nome"
+height = input('Qual é o seu pesso? ')
+Weight = input('Qual é a sua altura? ')
+
+float_height = float(height)
+float_Weight = float(Weight)
+
+imc = float_Weight / float_height ** 2
+result = f'{name} tem um imc de {imc:.2f}'
 print(result)
